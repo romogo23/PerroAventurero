111e7a5f92716bd3056d479a95cfc67f72d912a5
Ronald Mora Gómez <ronald.mora@ucrso.info>
2021-06-23 14:06:59 -0600

    Errores y validaciones

--- PerroAventurero/Models/EmpresasAfiliada.cs
+++ PerroAventurero/Models/EmpresasAfiliada.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -9,9 +10,15 @@ namespace PerroAventurero.Models
     {
         public int CodigoEmpresa { get; set; }
         public string Cedula { get; set; }
+        [Required(ErrorMessage = "Se requiere un nombre de empresa")]
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Nombre de empresa")]
         public string NombreEmpresa { get; set; }
+        [Required(ErrorMessage = "Se requiere un correo de empresa")]
+        [RegularExpression(@"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", ErrorMessage = "El correo no corresponde a uno válido")]
         public string Correo { get; set; }
         public byte[] Logo { get; set; }
+        [Required(ErrorMessage = "Se requiere una categoría")]
         public string Categoria { get; set; }
         public int? Telefono { get; set; }
 
