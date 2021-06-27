55ba485bb52b697fc30f751dd2c4f64d3cb5db15
Allan <allan.najera@gmail.com>
2021-06-26 20:15:48 -0600

    CRUD de eventos

---
+++ PerroAventurero/Controllers/EventoesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PerroAventurero.Models;
+
+namespace PerroAventurero.Controllers
+{
+    public class EventoesController : Controller
+    {
+        private readonly PAContext _context;
+
+        public EventoesController(PAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Eventoes
+        public async Task<IActionResult> Index()
+        {
+            var pAContext = _context.Eventos.Include(e => e.CedulaNavigation);
+            return View(await pAContext.ToListAsync());
+        }
+
+        // GET: Eventoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos
+                .Include(e => e.CedulaNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEvento == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // GET: Eventoes/Create
+        public IActionResult Create()
+        {
+            ViewData["Cedula"] = new SelectList(_context.UsuarioAdministradors, "Cedula", "Cedula");
+            return View();
+        }
+
+        // POST: Eventoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CodigoEvento,Cedula,NombreEvento,Lugar,Direccion,Fecha,PrecioGeneral,PrecioNinno,CantidadAforo,HoraInicio,HoraFinal,EnvioAnuncios,Comentarios")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(evento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Cedula"] = new SelectList(_context.UsuarioAdministradors, "Cedula", "Cedula", evento.Cedula);
+            return View(evento);
+        }
+
+        // GET: Eventoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            ViewData["Cedula"] = new SelectList(_context.UsuarioAdministradors, "Cedula", "Cedula", evento.Cedula);
+            return View(evento);
+        }
+
+        // POST: Eventoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CodigoEvento,Cedula,NombreEvento,Lugar,Direccion,Fecha,PrecioGeneral,PrecioNinno,CantidadAforo,HoraInicio,HoraFinal,EnvioAnuncios,Comentarios")] Evento evento)
+        {
+            if (id != evento.CodigoEvento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(evento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventoExists(evento.CodigoEvento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Cedula"] = new SelectList(_context.UsuarioAdministradors, "Cedula", "Cedula", evento.Cedula);
+            return View(evento);
+        }
+
+        // GET: Eventoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos
+                .Include(e => e.CedulaNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEvento == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // POST: Eventoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+            _context.Eventos.Remove(evento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventoExists(int id)
+        {
+            return _context.Eventos.Any(e => e.CodigoEvento == id);
+        }
+    }
+}
--- PerroAventurero/Models/Evento.cs
+++ PerroAventurero/Models/Evento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -14,15 +15,41 @@ namespace PerroAventurero.Models
 
         public int CodigoEvento { get; set; }
         public string Cedula { get; set; }
+        
+        [Required(ErrorMessage ="El nombre del evento es requerido")]
+        [MaxLength(80)]
         public string NombreEvento { get; set; }
+
+
+        [Required(ErrorMessage = "El lugar del evento es requerido")]
+        [MaxLength(100)]
         public string Lugar { get; set; }
-        public string Dirreccion { get; set; }
+
+
+        [Required(ErrorMessage = "La dirección del evento es requerida")]
+        [MaxLength(200)]
+        public string Direccion { get; set; }
+
+
+        [Required(ErrorMessage = "La fecha del evento es requerida")]
         public DateTime Fecha { get; set; }
+
+
+        [Required(ErrorMessage = "El precio de las entradas generales del evento es requerido")]
         public decimal PrecioGeneral { get; set; }
+
+        [Required(ErrorMessage = "El precio de las entradas de niños del evento es requerido")]
         public decimal PrecioNinno { get; set; }
+
+        [Required(ErrorMessage = "La cantidad del aforo del evento es requerida")]
         public int CantidadAforo { get; set; }
+
+        [Required(ErrorMessage = "La hora de inicio de entrada al evento es requerida")]
         public DateTime HoraInicio { get; set; }
+
+        [Required(ErrorMessage = "La hora de final de entrada al evento es requerida")]
         public DateTime HoraFinal { get; set; }
+
         public bool EnvioAnuncios { get; set; }
         public string Comentarios { get; set; }
 
