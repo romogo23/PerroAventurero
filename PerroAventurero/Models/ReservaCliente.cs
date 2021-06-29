d6c8831fed51a942f9427e5b83a3f9d5c3cac267
Alexandra Cordero <alexandra.cordero@ucrso.info>
2021-06-28 19:45:34 -0600

    Reserva 2

---
+++ PerroAventurero/Models/ReservaCliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerroAventurero.Models
+{
+    public class ReservaCliente
+    {
+        public Cliente cliente { get; set; }
+        public Reserva reserva { get; set; }
+
+        public List<Acompannante> acompannantes { set; get; }
+
+        public ReservaCliente() {
+            acompannantes = new List<Acompannante>();
+        }
+    }
+}
