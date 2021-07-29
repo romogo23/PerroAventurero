393e58987e6f14f7e5c95492a63629c80ce7b881
Alexandra Cordero <alexandra.cordero@ucrso.info>
2021-07-29 10:38:10 -0600

    Eliminar reservas vencidas

--- PerroAventurero/Controllers/HomeController.cs
+++ PerroAventurero/Controllers/HomeController.cs
@@ -26,9 +26,39 @@ namespace PerroAventurero.Controllers
         public async Task<IActionResult> Index()
         {
             ViewData["Nav"] = "true";
+            deleteReservas();
             var pAContext = _context.Eventos.Include(e => e.CedulaNavigation);
             return View(await pAContext.ToListAsync());
         }
+        private void deleteReservas() {
+            List<Evento> evento = _context.Eventos.Where(ev => ev.Fecha >= DateTime.Now).ToList();
+            for (int i = 0; i < evento.Count(); i++) {
+                dReserva(evento[i].CodigoEvento);
+            }
+        }
+
+
+        private void dReserva(int code) {
+            List<Reserva> reserva = _context.Reservas.Where(ev => ev.CodigoEvento >= code && ev.ComprobantePago == null && ev.FechaReserva.AddMinutes(20) <= DateTime.Now).ToList();
+            for (int i = 0; i < reserva.Count(); i++) {
+                dAcompanante(reserva[i].CodigoReserva);
+                _context.Reservas.Remove(reserva[i]);
+            }
+            _context.SaveChanges();
+        }
+
+        private void dAcompanante(int code)
+        {
+            List<Acompannante> acompannantes = _context.Acompannantes.Where(ev => ev.CodigoReserva == code).ToList();
+            for (int i = 0; i<acompannantes.Count(); i++) {
+                _context.Acompannantes.Remove(acompannantes[i]);
+                    
+            }
+            _context.SaveChanges();
+
+        }
+
+
 
         [Authorize]
         public IActionResult Aventuras()
